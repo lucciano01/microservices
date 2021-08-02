c0c48e4c8cd28433e9579337979e9c09900b5cf4
Luciano <lucciano01@hotmail.com>
2021-08-02 07:40:28 -0300

    ms greeting service

---
+++ greetin_service/src/main/java/com/luciano/luna/greetin/service/GreetinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.luna.greetin.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetinServiceApplication.class, args);
+    }
+
+}
---
+++ greetin_service/src/test/java/com/luciano/luna/greetin/service/GreetinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.luna.greetin.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
